8c7bbdf776b65fe7b755e54ffadb6d3555d3d498
Maciej Michalski <maciej.michalski@besidethepark.com>
2017-01-31 13:52:33 +0100

    updated tests

--- test/counties.test.js
+++ test/counties.test.js
@@ -5,7 +5,7 @@ require('should')
 describe('Counties', function () {
 
   it('should return counties for England', function () {
-    counties.england.length.should.equal(39)
+    counties.england.length.should.equal(50)
   })
 
   it('should return counties for Northern Ireland', function () {
@@ -13,15 +13,15 @@ describe('Counties', function () {
   })
 
   it('should return counties for Scotland', function () {
-    counties.scotland.length.should.equal(34)
+    counties.scotland.length.should.equal(38)
   })
 
   it('should return counties for Wales', function () {
-    counties.wales.length.should.equal(13)
+    counties.wales.length.should.equal(22)
   })
 
   it('should return all counties', function () {
-    counties.length.should.equal(92)
+    counties.length.should.equal(116)
   })
 
 })
